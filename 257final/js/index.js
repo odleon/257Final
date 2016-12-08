aa8dfd248b6100ccd8aaff54262f580d5d301b77
odleon <odleon@miamioh.edu>
2016-12-08 01:21:30 -0500

    Add files via upload

---
+++ 257final/js/index.js
@@ -0,0 +1,17 @@
+'use strict';
+
+
+    section = document.createElement('section'),
+    wrapper = document.createElement('div'),
+    article = document.createElement('article'),
+    date = document.createElement('date'),
+    header = document.createElement('header'),
+    button = document.createElement('button');
+
+template.appendChild(document.createElement('div').classList.add());
+
+imgs.forEach(function (item) {
+  
+});
+
+
